03106103990f0a6b5e43d099efa06c8fbca01ab5
Teddy <32977598+pdobrzynski@users.noreply.github.com>
2024-10-23 12:09:43 +0200

    Update events.example.spec.ts

--- tests/examples/events.example.spec.ts
+++ tests/examples/events.example.spec.ts
@@ -1,10 +1,8 @@
-import test, { expect } from "@playwright/test";
+import test, { expect, Locator } from "@playwright/test";
 
-test('wait for resposne', async ({page}) => {
-    const responsePromise = page.waitForResponse(response =>
-        response.url() === 'https://the-internet.herokuapp.com/slow_external' && response.status() === 503
-            && response.request().method() === 'GET'
-      );
+test('wait for response', async ({page}) => {
+    const responsePromise = page.waitForResponse(response => response.url() === 'https://the-internet.herokuapp.com/slow_external'
+        && response.status() === 503 && response.request().method() === 'GET')
     await page.goto('https://the-internet.herokuapp.com/slow')
     await responsePromise;
 
@@ -12,40 +10,44 @@ test('wait for resposne', async ({page}) => {
         const response = await Promise.all([
             page.waitForResponse(resp => resp.url().includes(url)
             && resp.status() === statusCode),
-            locator.click()
-        ]);
-        return response
+            locator.click() 
+        ])
     }
-    await clickAndWait(page.locator(''), '/slow', 503)
+
+    await clickAndWait(page.locator('#start'), '/slow_external', 503)
 })
 
 test('wait for load state', async ({page}) => {
-    await page.waitForLoadState('')
+    await page.waitForLoadState('networkidle')
 })
 
 test('wait for url', async ({page}) => {
     await page.goto('https://the-internet.herokuapp.com/redirector')
     await page.locator('#redirect').click()
-    await page.waitForURL('https://the-internet.herokuapp.com/status_codes');
+    await page.waitForURL('https://the-internet.herokuapp.com/status_codes')
     console.log('Redirected.')
 })
 
+test('wait for event', async ({page}) => {
+    const dialogPromise = await page.waitForEvent('dialog')
+    dialogPromise.accept()
+})
 
-test('events', async ({page}) => {
-    // podobne jak waitForEvents ale mniej intuicyjne
+test('listener', async ({page}) => {
     page.on('dialog', async dialog => {
         console.log(dialog.accept())
     })
+
     await page.goto('https://the-internet.herokuapp.com/javascript_alerts')
-    await page.getByRole('button', {name: 'Click for JS Confirm'}).click()
+    await page.getByRole('button', {name: 'Click for JS Confirm'}).click();
     await expect(page.locator('#result')).toHaveText('You clicked: Ok')
 })
 
-test('page.on console', async ({page}) => {
-    page.on('console', msg => {
+test('listener - console', async ({page}) => {
+    page.on('console', async msg => {
         if (msg.type() === 'error') {
-          console.log('Błąd konsoli:', msg.text());
+            console.log('Błąd konsoli: ', msg.text())
         }
-      });
+    })
     await page.goto('https://the-internet.herokuapp.com/javascript_error')
 })
