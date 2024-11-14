12d855c045560101acf02d9a64f903ed333806b0
Teddy <32977598+pdobrzynski@users.noreply.github.com>
2024-11-14 19:13:24 +0100

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,9 @@ export default defineConfig<TestUser>({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['./reporter/my-reporter.ts', {customOptions: 'Parmetrized Tests'}]],
+    reporter: [['./reporter/my-reporter.ts', {
+    reportFileName: 'report-parametrized-tests'
+  }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     browserName: 'chromium',
